76f08538bab60bfc44ef295d09c467e5d2e26b0a
Haltz01 <pedro-x-guerra@hotmail.com>
2021-05-20 13:15:52 -0300

    Adding homepage template + header

--- frontend/src/pages/Homepage/Homepage.js
+++ frontend/src/pages/Homepage/Homepage.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
 import './Homepage.css';
-import HomeHeader from '../components/Headers/HomeHeader'
+import HomeHeader from '../components/Headers/HomeHeader';
+
+// Images
+import chickenImg from '../../images/chicken_homepage.png';
 
 export default function Homepage() {
     return (
         <React.Fragment>
             <HomeHeader />
-            <h1> Hello World </h1>
+            <div className="container gradiantMainContainer">
+                <div className="row d-flex justify-content-center py-4">
+                    <div className="col">
+                        <h1 className="homepageBrandName"> T A M A G O V O </h1>
+                        <p className="homepageMainText">Conheça o maior mercado de ovos de todo o Brasil!</p>
+                        <p className="homepageMainText">A TAMAGOVO trabalha com ovos de qualidade de todos os tipos, com grande variedade e preço bom.</p>
+                        <p className="homepageMainText">Aqui você encontra ovos de galinha, de cordorna, de avestruz, ovos centenários, exóticos e até mesmo ovo em pó!</p>
+                    </div>
+                    <div className="col">
+                        <img className="img-fluid" src={chickenImg} alt="Galinha gigante simpática" />
+                    </div>
+                </div>
+                <div className="row">
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                </div>
+            </div>
         </React.Fragment>
     );
 }
--- frontend/src/pages/components/Headers/HomeHeader.js
+++ frontend/src/pages/components/Headers/HomeHeader.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './HomeHeader.css';
+
+// Images
+import EggLogo from '../../../images/egg_logo_example.png';
 
 class HomeHeader extends React.Component {
     render() {
         return (
-            <h2> I'm a header </h2>
+            <div className="navbar navbar-expand-lg homepageHeader sticky-top py-2 px-4">
+                <Link className="navbar-brand d-flex justify-content-center align-items-center" to="/"><img className="img-fluid headerLogo" src={EggLogo} alt="Egg Logo"/>
+                    <span className="headerBrandName p-2"> T A M A G O V O </span>
+                </Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse d-flex align-items-center justify-content-end" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto d-flex justify-content-center">
+                        <li class="nav-item active">
+                            <Link className="nav-link navBarLink p-2 mx-2" to="/">Produtos</Link>
+                        </li>
+                        <li class="nav-item active">
+                            <Link className="nav-link navBarLink p-2 mx-2" to="/">Sobre nós</Link>
+                        </li>
+                        <li class="nav-item active">
+                            <Link className="nav-link navBarLink p-2 mx-2" to="/">Contato</Link>
+                        </li>
+                        <li class="nav-item active">
+                            <Link className="nav-link navBarLinkSpecial p-2 mx-2" to="/">Compre agora</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         );
     }
 }
