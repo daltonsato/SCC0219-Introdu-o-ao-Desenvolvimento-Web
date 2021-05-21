c0ae56524a4b2add6b48e7704b66844c120aba15
Haltz01 <pedro-x-guerra@hotmail.com>
2021-05-21 16:41:54 -0300

    Minor fixes in shopping cart

--- frontend/src/pages/ShoppingCart/ShoppingCart.js
+++ frontend/src/pages/ShoppingCart/ShoppingCart.js
@@ -1,20 +1,17 @@
 import React from 'react';
 
-import './Homepage.css';
+import './ShoppingCart.css';
 import HomeHeader from '../components/Headers/HomeHeader';
-
-// Images
-import chickenImg from '../../images/chicken_homepage.png';
 
 export default function ShoppingCart() {
     return (
-        <React.Fragment>
+        <div className="gradiantMainContainer">
             <HomeHeader />
-            <div className="container gradiantMainContainer">
+            <div className="container p-4">
 				<div className="row d-flex justify-content-between py-4">
 					<div className="col">
-						<h1 className="shoppingCart text-center py-3">Meu carrinho de compras</h1>
-						<div className="row d-flex justify-content-evenly py-1 px-4 generalBox">
+						<h1 className="shoppingCart text-center py-3 mb-3">Meu carrinho de compras</h1>
+						<div className="row d-flex justify-content-evenly py-1 generalBox">
 							<div className="col p-3 atributteDisplay justify-content-center">
 								Nome do produto
 							</div>
@@ -31,17 +28,17 @@ export default function ShoppingCart() {
 					</div>
 				</div>
 				<div className="row">
-					<div className="generalBox"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></div>
+					<div className="col generalBox"><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/></div>
 				</div>
-				<div className="row d-flex justify-content-end">
-					<div className="generalBox float-end col-3 px-3 py-3">Total a pagar:</div>
+				<div className="row d-flex justify-content-end align-items-center py-3">
+					<div className="col-3 generalBox text-center p-3">Total a pagar:</div>
 				</div>
-					<div className="finishShop text-center py-3">
-						<div className="finishShopF">
-							FINALIZAR COMPRA
-						</div>
+				<div className="row finishShop text-center ">
+					<div className="col finishShopF py-3">
+						FINALIZAR COMPRA
 					</div>
+				</div>
 			</div>
-        </React.Fragment>
+        </div>
     );
 }
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/Homepage/Homepage';
+import ShoppingCart from './pages/ShoppingCart/ShoppingCart';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path={`/`} exact component={ Homepage }/> {/* sem o exact, o router não irá diferenciar essa das outras Routes, pois o Router compara o começo da rota somente (startsWith)*/ }
-				<Route path={'/shoppingCart'} exact component={ShoppingCart}/>
+				<Route path={'/shopping-cart'} exact component={ShoppingCart}/>
             </Switch>
         </BrowserRouter>
     );
