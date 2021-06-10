7191fe6bbe1be3df17f94fde4b41aea50b000575
VitorAmim <stahitsky@gmail.com>
2021-06-10 18:58:39 -0300

    Store design update

--- frontend/src/pages/Store/Store.js
+++ frontend/src/pages/Store/Store.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './Store.css';
 import HomeHeader from '../components/Headers/HomeHeader';
+import Footer from '../components/Footer/Footer';
 
 // Images
 
-import ovo from '../../images/ovo.png';
+import ovo from '../../images/ovos.png';
+import ovoPo from '../../images/ovoPo.png';
 
 
 
@@ -14,59 +16,129 @@ export default function Store() {
         <React.Fragment>
             <HomeHeader />
             <div className="backGround">
-                <div className = "container p-4">
+                <div className = "container">
 
 
-                <div className="row d-flex justify-content-end align-items-center py-3">
-                        <h3 className = "p-3 col">Todos os produtos</h3>
-                        <h5 className="py-3 col-3 text-center searchBar">Pesquisar produto</h5>
-                </div>
+                    <div className="d-flex justify-content-end align-items-center pt-4">
+                            <h1 className = "py-3 col">Produtos</h1>
+                            <h5 className="py-3 col-3 text-center searchBar">Pesquisar produto</h5>
+                    </div>
+                    <p>Conheça nosso catálogo</p>
+           
 
 
-                    <div className = "row justify-content-around productBar">
-                            <div className = "col">
-                                <div className = "productContainer ">
-                                    <img className="img-fluid w-50 rounded mx-auto d-block" src={ovo} alt="ovo" />
-                                    <h6> Ovo milenar</h6>
-                                    <h6 className = "pb-2"> Preço: 1000 R$</h6>
-                                    <div className = "add mx-auto  w-50">
-                                         <h6>Adicionar ao carrinho</h6>
-                                    </div>
+                  
+                    <h3 className = "pt-7 pb-5 d-flex justify-content-center category">Ovos caipira</h3>
+                    <div className = "row d-flex justify-content-start align-items-start productBar pb-5">
+                        <div className = "col-3">
+                            <div className = "productContainer">
+                                <h6 > Ovo caipira</h6>
+                                <img className="img-fluid w-50 rounded mx-auto d-block py-4" src={ovo} alt="ovo" />
+                                <h6 className = "pb-2"> Preço: 1000 R$</h6>
+                                <div className = "buy mx-auto  w-50">
+                                        <h6 className = "py-2">Comprar</h6>
                                 </div>
                             </div>
-                            <div className = "col">
-                                <div className = "productContainer ">
-                                    <img className="img-fluid w-50 rounded mx-auto d-block" src={ovo} alt="ovo" />
-                                    <h6> Ovo milenar</h6>
-                                    <h6 className = "pb-2"> Preço: 1000 R$</h6>
-                                    <div className = "add mx-auto  w-50">
-                                         <h6>Adicionar ao carrinho</h6>
-                                    </div>
+                        </div>
+                        <div className = "col-3">
+                            <div className = "productContainer">
+                                <h6 > Ovo caipira</h6>
+                                    <img className="img-fluid w-50 rounded mx-auto d-block py-4" src={ovo} alt="ovo" />
+                                <h6 className = "pb-2"> Preço: 1000 R$</h6>
+                                <div className = "buy mx-auto  w-50">
+                                    <h6 className = "p-2">Comprar</h6>
                                 </div>
                             </div>
-                            <div className = "col">
-                                <div className = "productContainer ">
-                                    <img className="img-fluid w-50 rounded mx-auto d-block" src={ovo} alt="ovo" />
-                                    <h6> Ovo milenar</h6>
-                                    <h6 className = "pb-2"> Preço: 1000 R$</h6>
-                                    <div className = "add mx-auto w-50">
-                                         <h6>Adicionar ao carrinho</h6>
-                                    </div>
+                        </div>
+                        <div className = "col-3">
+                            <div className = "productContainer">
+                                <h6 > Ovo caipira</h6>
+                                <img className="img-fluid w-50 rounded mx-auto d-block py-4" src={ovo} alt="ovo" />
+                                <h6 className = "pb-2"> Preço: 1000 R$</h6>
+                                <div className = "buy mx-auto  w-50">
+                                    <h6 className = "p-2">Comprar</h6>
                                 </div>
                             </div>
+                        </div>
+                        <div className = "col-3">
+                            <div className = "productContainer">
+                                <h6 > Ovo caipira</h6>
+                                <img className="img-fluid w-50 rounded mx-auto d-block py-4" src={ovo} alt="ovo" />
+                                <h6 className = "pb-2"> Preço: 1000 R$</h6>
+                                <div className = "buy mx-auto  w-50">
+                                    <h6 className = "p-2">Comprar</h6>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
-                
 
-                    <h3 className = "pt-5 pb-3">Em promoção</h3>
-                    <div className = "row justify-content-around productBar">
-                        <div className = "col">
+                    <h3 className = "pt-7 pb-5 d-flex justify-content-center category">Ovos Brancos</h3>
+                    <div className = "row d-flex justify-content-start align-items-start productBar pb-5">
+                        <div className = "col-3">
                             <div className = "productContainer">
-                                <img className="img-fluid w-50 rounded mx-auto d-block" src={ovo} alt="ovo" />
-                                <h6> Ovo milenar</h6>
+                                <h6 > Ovo branco</h6>
+                                <img className="img-fluid w-50 rounded mx-auto d-block py-4" src={ovo} alt="ovo" />
                                 <h6 className = "pb-2"> Preço: 1000 R$</h6>
-                                <div className = "add mx-auto  w-50">
-                                        <h6>Adicionar ao carrinho</h6>
+                                <div className = "buy mx-auto  w-50">
+                                        <h6 className = "p-2">Comprar</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className = "col-3">
+                            <div className = "productContainer">
+                                <h6 > Ovo branco</h6>
+                                <img className="img-fluid w-50 rounded mx-auto d-block py-4" src={ovo} alt="ovo" />
+                                <h6 className = "pb-2"> Preço: 1000 R$</h6>
+                                <div className = "buy mx-auto  w-50">
+                                    <h6 className = "p-2">Comprar</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className = "col-3">
+                            <div className = "productContainer">
+                                <h6 > Ovo branco</h6>
+                                <img className="img-fluid w-50 rounded mx-auto d-block py-4" src={ovo} alt="ovo" />
+                                <h6 className = "pb-2"> Preço: 1000 R$</h6>
+                                <div className = "buy mx-auto  w-50">
+                                    <h6 className = "p-2">Comprar</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className = "col-3">
+                            <div className = "productContainer">
+                                <h6 > Ovo branco</h6>
+                                <img className="img-fluid w-50 rounded mx-auto d-block py-4" src={ovo} alt="ovo" />
+                                <h6 className = "pb-2"> Preço: 1000 R$</h6>
+                                <div className = "buy mx-auto  w-50">
+                                    <h6 className = "p-2">Comprar</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                
+                    <h3 className = "pt-7 pb-5 d-flex justify-content-center category">Ovos em pó</h3>
+                    <div className = "row d-flex justify-content-start align-items-start productBar pb-5">
+                        <div className = "col-3">
+                            <div className = "productContainer">
+                                <h6 > Ovo em Pó</h6>
+                                <img className="img-fluid w-50 rounded mx-auto d-block py-3" src={ovoPo} alt="ovo" />
+                                <h6 className = "pb-2"> Preço: 1000 R$</h6>
+                                <div className = "buy mx-auto  w-50">
+                                        <h6 className = "p-2">Comprar</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className = "col-3">
+                            <div className = "productContainer">
+                                <h6 > Ovo em Pó</h6>
+                                <img className="img-fluid w-50 rounded mx-auto d-block py-3" src={ovoPo} alt="ovo" />
+                                <h6 className = "pb-2"> Preço: 1000 R$</h6>
+                                <div className = "buy mx-auto  w-50">
+                                    <h6 className = "p-2">Comprar</h6>
                                 </div>
                             </div>
                         </div>
@@ -74,6 +146,7 @@ export default function Store() {
 
                     
                 </div>
+                <Footer />
             </div>
         </React.Fragment>
     );
