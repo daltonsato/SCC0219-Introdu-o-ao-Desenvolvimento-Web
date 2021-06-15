d88150114250a7424968b39cf96d880a7ca5a7e2
daltonsato <daltonhiroshisato@hotmail.com>
2021-06-14 21:18:11 -0300

    Route updated. Payment and AddAddress

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
 import ShoppingCart from './pages/ShoppingCart/ShoppingCart';
 import Store from './pages/Store/Store';
+import Payment from './pages/Payment/Payment';
 import AboutUs from './pages/AboutUs/AboutUs';
 import ContactUs from './pages/ContactUs/ContactUs';
 import Login from './pages/Login/Login';
@@ -22,6 +23,7 @@ export default function Routes() {
                 <Route path={'/contact-us'} exact component={ContactUs}/>
 				<Route path={'/shopping-cart'} exact component={ShoppingCart}/>
                 <Route path={'/store'} exact component={Store}/>
+				<Route path={'/payment'} exact component={Payment}/>
                 <Route path={'/login'} exact component={Login}/>
                 <Route path={'/register'} exact component={Register}/>
 				<Route path={'/add-address'} exact component={AddAddress}/>
