3809157fd9a7253bcebf86c45ea4911efdab6afd
daltonsato <daltonhiroshisato@hotmail.com>
2021-06-23 10:53:02 -0300

    Agora responsivo

--- frontend/src/pages/Payment/Payment.js
+++ frontend/src/pages/Payment/Payment.js
@@ -12,12 +12,12 @@ export default function Payment() {
 			<h1 className="display-1 mx-5 my-5 px-5 py-5">Pagamento</h1>
             <div className="container d-flex align-itens-center eggBackground">
 				<div className="col d-flex flex-column justify-content-center">
-					<div className="row d-flex flex-column col-4 dataBox align-self-center">
+					<div className="row d-flex flex-column col-sm-8 col-md-6 col-xl-4 col-4 dataBox align-self-center">
 						<form className="d-flex flex-column align-items-center justify-content-center">
-							<input className="inputData row col-8 my-4 mt-5 p-3" type="nome" id="nome" name="nome" placeholder="Nome" required></input>
-							<input className="inputData row col-8 my-4 p-3" type="cpf" id="cpf" name="cpf" placeholder="CPF" required></input>
-							<input className="inputData row col-8 my-4 p-3" type="cartaoNumero" id="cartaoNumero" name="cartaoNumero" placeholder="Número do cartão" required></input>
-							<input className="inputData row col-8 my-4 mb-5 p-3" type="titular" id="titular" name="titular" placeholder="Nome do titular" required></input>
+							<input className="inputData row col-8 col-sm-8 col-md-8 my-4 mt-5 p-3" type="nome" id="nome" name="nome" placeholder="Nome" required></input>
+							<input className="inputData row col-8 col-sm-8 col-md-8 my-4 p-3" type="cpf" id="cpf" name="cpf" placeholder="CPF" required></input>
+							<input className="inputData row col-8 col-sm-8 col-md-8 my-4 p-3" type="cartaoNumero" id="cartaoNumero" name="cartaoNumero" placeholder="Número do cartão" required></input>
+							<input className="inputData row col-8 col-sm-8 col-md-8 my-4 mb-5 p-3" type="titular" id="titular" name="titular" placeholder="Nome do titular" required></input>
 						</form>
 					</div>
 					<Link className="row justify-content-center col-2 confirmInfo my-5 mx-3 p-3 align-self-center" to="">
