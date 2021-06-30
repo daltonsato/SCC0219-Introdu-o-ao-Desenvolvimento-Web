83e68d63636a806e362d1e8917cd54894a17a6ad
daltonsato <daltonhiroshisato@hotmail.com>
2021-06-30 16:05:53 -0300

    It works!! Not responsive yet

--- frontend/src/pages/ShoppingCart/ShoppingCart.js
+++ frontend/src/pages/ShoppingCart/ShoppingCart.js
@@ -35,7 +35,7 @@ export default function ShoppingCart() {
 				console.log(count);
 			}
 		}
-	};*/
+	};
 	function increase(name){
 		for(const [prodName, prodDetails] of Object.entries(window.shoppingCart)){
 			if(name === prodName){
@@ -51,6 +51,25 @@ export default function ShoppingCart() {
 				console.log(prodDetails.quantity);
 			}
 		}
+	}*/
+	let increase = (props) =>{
+		let productID = props.target.parentNode.id;
+
+		window.shoppingCart[productID].quantity += 1;
+		props.target.parentNode.childNodes[0].value = window.shoppingCart[productID].quantity;
+		setCount(props.target.parentNode.childNodes[0].value);
+		console.log(window.shoppingCart[productID]);
+	}
+
+	let decrease = (props) =>{
+		let productID = props.target.parentNode.id;
+		
+		if(window.shoppingCart[productID].quantity > 0){
+			window.shoppingCart[productID].quantity -= 1;
+			props.target.parentNode.childNodes[0].value = window.shoppingCart[productID].quantity;
+			setCount(props.target.parentNode.childNodes[0].value);
+			console.log(window.shoppingCart[productID]);
+		}
 	}
 
 	let testCookie = "280E8410C4A05326EB815B577B05574FDFB4AE016C399ACF1B02CFE5C59D59FE"; // sha-256 -> ganeshtestlogin (used for testing)
@@ -63,16 +82,16 @@ export default function ShoppingCart() {
 		let totalApagar = 0;
 		for (const [prodName, prodDetails] of Object.entries(window.shoppingCart)) {
 			itensList.push( <div key={"div_"+prodName} className="row">
-				<div className="col-3 p-3 atributteDisplay justify-content-center border primary-border"> {prodDetails.prodName} </div>
-				<div className="col-3 p-3 atributteDisplay justify-content-center border primary-border">
-				<input className="Quantity" id="inputGroup-sizing-sm" type="number" name="clicks" value={prodDetails.quantity} onChange={(event) => {
-					setCount(event.target.value);
-				}} /> 
-				<button className="col btn" onClick={increase(prodName)}>+</button>
-				<button className="col btn" onClick={decrease(prodName)}>-</button>
+				<div className="col-3 p-3 atributteDisplay justify-content-center"> {prodDetails.prodName} </div>
+				<div className="col-3 p-3 atributteDisplay justify-content-center" id={prodName}>
+					<input className="Quantity" id="inputGroup-sizing-sm" type="number" name="clicks" value={prodDetails.quantity} onChange={(event) => {
+					}} /> 
+					<button className="col btn" onClick={increase}>+</button>
+					<button className="col btn" onClick={decrease}>-</button>
+					{console.log(prodDetails.quantity)}
 				</div>
-				<div className="col-3 p-3 atributteDisplay justify-content-center border primary-border"> R$ {prodDetails.price} </div>
-				<div className="col-3 p-3 atributteDisplay justify-content-center border primary-border"> R$ {prodDetails.price*prodDetails.quantity} </div></div> );
+				<div className="col-3 p-3 atributteDisplay justify-content-center"> R$ {prodDetails.price} </div>
+				<div className="col-3 p-3 atributteDisplay justify-content-center"> R$ {prodDetails.price*prodDetails.quantity} </div></div> );
 
 				totalApagar += prodDetails.price*prodDetails.quantity;
 		}
@@ -84,13 +103,13 @@ export default function ShoppingCart() {
 						<h1 className="shoppingCart text-center py-3 mb-3">Meu carrinho de compras</h1>
 						<div className="row d-flex justify-content-evenly py-1 generalBox">
 							<div className="col-3 p-3 atributteDisplay justify-content-center">
-								Nome do produto
+								Nome
 							</div>
 							<div className="col-3 p-3 atributteDisplay justify-content-center">
 								Qtd
 							</div>
 							<div className="col-3 p-3 atributteDisplay justify-content-center">
-								Preço unidade
+								Preço
 							</div>
 							<div className="col-3 p-3 atributteDisplay justify-content-center">
 								Total
@@ -104,7 +123,7 @@ export default function ShoppingCart() {
 					</div>
 				</div>
 				<div className="row d-flex justify-content-end py-3">
-					<div className="col-3 totalPay py-4 text-left">Total a pagar:   R${totalApagar}</div>
+					<div className="col-8 col-sm-6 col-md-5 totalPay py-4 text-left">Total a pagar:   R${totalApagar}</div>
 				</div>
 				<div className="row finishShop text-center ">
 					<button className="col finishShopF py-3" onClick={goToAddressSel}>
